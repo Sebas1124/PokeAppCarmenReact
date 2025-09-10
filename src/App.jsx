11539959246e2f097e3086f12d1a303eee9e311e
Sebastian <Srosero87@misena.edu.co>
2025-09-10 16:59:56 +0200

    Cambios en los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,73 @@ export const App = () => {
 
   const pokemons = [
     {
+      id: 1,
       name: "bulbasaur",
-      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
       favorite: true
     },
     {
+      id: 2,
       name: "ivysaur",
-      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
+      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
     },
     {
+      id: 3,
       name: "venusaur",
-      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg",
+      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
     },
     {
+      id: 4,
       name: "charmander",
-      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg",
-    }
+      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      id: 5,
+      name: "charmeleon",
+      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+    },
+    {
+      id: 6,
+      name: "charizard",
+      img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+    },
   ]
+
+  /* SPA -> Single Page Application */
+  /* NPM -> Node Package Manager */
 
   return (
     <div>
-      {
-        pokemons.map((pokemon) => (
-          <PokemonCard
-            img={pokemon.img}
-            name={pokemon.name}
-            pokemon={pokemon}
-          />
-        ))
-      }
-      
+
+      <header className="logo-container">
+  
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/5/51/Pokebola-pokeball-png-0.png" 
+          alt="Logo PokeApp"
+          className="logo" 
+        />
+        <h1>PokeApp</h1>
+
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Buscar pokémon..."
+        />
+
+      </header>
+
+      <main className="pokedex-grid">
+        {
+          pokemons.map((pokemon) => (
+            <PokemonCard
+              img={pokemon.img}
+              name={pokemon.name}
+              pokemon={pokemon}
+            />
+          ))
+        }
+        
+      </main>
     </div>
   )
 }
--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -6,16 +6,31 @@ export const PokemonCard = ({ img, name, pokemon }) => {
     }
 
   return (
-    <div className="pokemon-card" style={{ 
-        backgroundColor: pokemon.favorite ? 'blue' : "black"
-    }}>
-        <img 
-            className="pokemon-image" 
-            src={img}
-            alt="pokemon"
-        />
-        <h2 className="pokemon-name">{name}</h2>
-        <button onClick={() => handleFavorite(pokemon)}>❤️</button>
+    <div className={`pokemon-card fire-type`}>
+        <div className="card-inner">
+            <div className="card-front">
+                <span className="pokemon-id">#001</span>
+                <img 
+                    className="pokemon-image" 
+                    src={`${img}`} 
+                    alt={name}
+                />
+                <h2 className="pokemon-name">{name}</h2>
+                <div className="pokemon-type-container">
+                    {/* {pokemon.types.map(({ type }) => (
+                        <div key={type.name} className={`type-badge ${type.name}`}>{type.name}</div>
+                    ))} */}
+                </div>
+            </div>
+            <div className="card-back">
+                <h3>Habilidades</h3>
+                <ul>
+                    <li>Overgrow</li>
+                    <li>Chlorophyll</li>
+                    {/* {pokemon.abilities.map(({ ability }) => <li key={ability.name}>{ability.name}</li>)} */}
+                </ul>
+            </div>
+        </div>
     </div>
   )
 }
